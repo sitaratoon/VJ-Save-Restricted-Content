ebcc25e798d16b914b11bf65957e23d28633a35a
sitaratoon <ajay393798@gmail.com>
2025-01-30 18:50:34 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -60,7 +60,9 @@ def progress(current, total, message, type):
 async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
-    buttons = [   InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/Anime_Chats_St'),
+    buttons = [
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+    ], [   InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/Anime_Chats_St'),
         InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/ST_Rename_Update')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
